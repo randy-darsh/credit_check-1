d9fcad670fa412362ba324a2effcac4ad8653401
rspringer47 <rspringer47@gmail.com>
2017-06-29 14:06:29 -0600

    removed unnecessary lines

--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -19,7 +19,6 @@ class CreditCheckTest < Minitest::Test
 
   def test_it_doubles_every_number
     check = CreditCheck.new("6011797668867828")
-    check.split_number
 
     assert_equal ["12", "0", "2", "1", "14", "9", "14", "6",
                   "12", "8", "16", "6", "14", "8", "4", "8"], check.double
@@ -27,7 +26,6 @@ class CreditCheckTest < Minitest::Test
 
   def test_it_can_add_doubled_numbers_over_10_together
     check = CreditCheck.new("6011797668867828")
-    check.split_number
 
     assert_equal ["3", "0", "2", "1", "5", "9", "5", "6",
                   "3", "8", "7", "6", "5", "8", "4", "8"], check.reduce_doubled_numbers
